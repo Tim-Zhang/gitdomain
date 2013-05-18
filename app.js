dd58c43a9220f038860ecb08fdc547af99059698
TimZhang <timzhang@dnspod.com>
2013-05-18 15:34:19 +0000

    add user agent judement

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ var http = require('http')
   var port = 10000;
 
   var router = new director.http.Router();
-  router.get('/notify/:id', notify);
+  router.post('/notify/:id', notify);
 
   function notify(id) {
     this.res.writeHead(200, { 'Content-Type': 'text/plain' })
-    db.getUser(id, function(err, doc) {
-      var user = doc[0];
+    if (this.req.headers['user-agent'] !== 'GitHub Hookshot 8678377') {
+       this.res.end('github only');
+    } else {
+       db.getUser(id, function(err, doc) {
+         var user = doc[0];
 
-      if (user && user.gitrep && user.access_token)  {
-        core.dnspod(id, user.gitrep, user.lastrep, user.access_token); 
-        console.log(id, ': done');
-      }
-    }); 
-    this.res.end(id);
+         if (user && user.gitrep && user.access_token)  {
+           core.dnspod(id, user.gitrep, user.lastrep, user.access_token); 
+           console.log(id, ': done');
+         }
+       }); 
+       this.res.end(id + ': done');
+    }
   }
 
   var server = http.createServer(function (req, res) {
